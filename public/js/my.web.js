6fd14e66ff6be1cf9695f4c8b6882bcbea396349
Mehrzad@1 <mehrzadtajkarimi@gmail.com>
2019-02-10 10:26:23 +0330

    یکشنبه 21 بهمن 10:26

--- public/js/my.web.js
+++ public/js/my.web.js
@@ -1,1 +1,65 @@
-$(window).scroll(function () {    if ($(window).scrollTop() > 50) {        $("#nav").css({backgroundColor: "#fff"});        $("#nav a").css({color: "#999"});    } else {        $("#nav").css({backgroundColor: "transparent"});        $("#nav a ").css({color: "#fff"});    }});//////////////////////////////////////////////////////////////////////////////////////////$("#header-index #nav #burger").click(function () {    $(this).toggleClass("open");    $('body').toggleClass("open-nav");});//////////////////////////////////////////////////////////////////////////////////////////$(document).ready(function () {    $(".movieh").hover3d({        selector: ".movieh__card",        perspective: 10000,        sensitivity: 150    });});//////////////////////////////////////////////////////////////////////////////////////////var width = $(window).width() / 1.8;$("#banner-big-bg .movieh .movieh__card").css({height: width});$(window).resize(function () {    var width = $(window).width() / 1.8;    $("#banner-big-bg .movieh .movieh__card").css({height: width});});//////////////////////////////////////////////////////////////////////////////////////////wow = new WOW(        {            boxClass: 'wow', // default            animateClass: 'animated', // default            offset: 100, // default            mobile: false, // default            live: true        // default        })wow.init();//////////////////////////////////////////////////////////////////////////////////////////var typed = new Typed("#typing", {    strings: [        "طراحی وب سایت   مطابق با استاندارد های جهانی"    ],    typeSpeed: 60,    startDelay: 4000,    showCursor: false});//////////////////////////////////////////////////////////////////////////////////////////$("#top").click(function () {    $("html,body").animate({scrollTop: 0}, 500);});$(window).scroll(function () {    if ($(window).scrollTop() > 200) {        $("#top").fadeIn(500);    } else {        $("#top").fadeOut(500);    }});//////////////////////////////////////////////////////////////////////////////////////////    $(".flipTimer").flipTimer({                direction:'down',                date:'January 31,2019 17:47:00'});
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 50) {
+    $("#nav").css({backgroundColor: "#fff"});
+    $("#nav a").css({color: "#999"});
+  } else {
+    $("#nav").css({backgroundColor: "transparent"});
+    $("#nav a ").css({color: "#fff"});
+  }
+});
+//////////////////////////////////////////////////////////////////////////////////////////
+$("#header-index #nav #burger").click(function () {
+  $(this).toggleClass("open");
+  $('body').toggleClass("open-nav");
+});
+//////////////////////////////////////////////////////////////////////////////////////////
+$(document).ready(function () {
+  $(".movieh").hover3d({
+    selector: ".movieh__card",
+    perspective: 10000,
+    sensitivity: 150
+  });
+});
+//////////////////////////////////////////////////////////////////////////////////////////
+var width = $(window).width() / 1.8;
+$("#banner-big-bg .movieh .movieh__card").css({height: width});
+$(window).resize(function () {
+  var width = $(window).width() / 1.8;
+  $("#banner-big-bg .movieh .movieh__card").css({height: width});
+});
+//////////////////////////////////////////////////////////////////////////////////////////
+wow = new WOW(
+  {
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 100, // default
+    mobile: false, // default
+    live: true        // default
+  }
+)
+wow.init();
+//////////////////////////////////////////////////////////////////////////////////////////
+var typed = new Typed("#typing", {
+  strings: [
+    "طراحی وب سایت   مطابق با استاندارد های جهانی"
+  ],
+  typeSpeed: 60,
+  startDelay: 4000,
+  showCursor: false
+});
+//////////////////////////////////////////////////////////////////////////////////////////
+$("#top").click(function () {
+  $("html,body").animate({scrollTop: 0}, 500);
+});
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 200) {
+    $("#top").fadeIn(500);
+  } else {
+    $("#top").fadeOut(500);
+  }
+});
+//////////////////////////////////////////////////////////////////////////////////////////
+$(".flipTimer").flipTimer({
+  direction:'down',
+  date:'January 31,2019 17:47:00'
+});
