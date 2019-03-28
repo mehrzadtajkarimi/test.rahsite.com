7d642859ac8a7cb0f501933be5954b06935fa1df
Mehrzad@1 <mehrzadtajkarimi@gmail.com>
2019-03-28 15:37:29 +0430

    start

---
+++ views/categorizeArticles/site_design.php
@@ -0,0 +1,40 @@
+<?php
+$result  =$data['article'];
+$timeLogs=$data['timeLogs'];
+foreach ($result as $row) {
+    ?>
+    <div class="card border-0 shadow-sm mb-4">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title font-face"><?= $row['title']; ?></h5>
+                    <p class="card-text"><?= $row['summary']; ?></p>
+                    <a href="<?= URL ?>article/index/<?= $row['id']; ?>" type="button" class="btn btn-outline-secondary mt-4">
+                        ادامه مطالب</a>
+
+
+
+                    <div class="blog-date float-left text-muted pl-3 pb-3 ">
+                        <div class="date ">
+                            <div class="date-cont">
+                                <span class="day"><?= $timeLogs['day']; ?></span>
+                                <span class="month"><span class="text-center"><?= $timeLogs['months'];?></span></span>
+                                <span class="year"><?= $timeLogs['year'];?></span>
+                                <i class="springs"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="public\image\<?= $row['image_small']; ?>" class="img-fluid"
+                     alt="<?= $row['image_small']; ?>"
+                     title="<?= $row['image_small']; ?>">
+            </div>
+
+        </div>
+
+    </div>
+<?php } ?>
+
