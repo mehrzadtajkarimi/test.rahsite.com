02aa5aa11df7a3927e8b3f5b411568d875abdfc4
Mehrzad@1 <mehrzadtajkarimi@gmail.com>
2019-04-09 19:45:58 +0430

    afzodan ghsmat nazarat ba ajax dar ghesmat maghalat

--- controllers/article.php
+++ controllers/article.php
@@ -1,33 +1,60 @@
 <?php
-class article extends Controller
-{
-  function __construct()
-  {
 
-  }
-  function index($id_articles,$id_articles_category){
+class article extends Controller {
 
-    $article=$this->model->articleInfo($id_articles);
-    $articlesCategorize=$this->model->articlesCategorize();
-    $mostView=$this->model->mostView();
-    $newArticle=$this->model->newArticle();
+    function __construct() {
 
-    $commentParameter=$this->model->commentParameter($id_articles,$id_articles_category);
-    $commentParameterName=$commentParameter['titel'];
-    $commentParameterScores=$commentParameter['scores_total'];
+    }
 
-    $comments=$this->model->comment($id_articles,$id_articles_category);
-    $data=[
-      'article'=>$article,
-      'articlesCategorize'=>$articlesCategorize,
-      'mostView'=>$mostView,
-      'newArticle'=>$newArticle,
-      'commentParameterName'=>$commentParameterName,
-      'commentParameterScores'=>$commentParameterScores,
+    function index($id_articles, $id_articles_category) {
 
-      'comments'=>$comments
-    ];
-    $this->view('articles/index',$data);
+        $article = $this->model->articleInfo($id_articles);
+        $article_fetchAll = $article['article_fetchAll'];
+        $article_fetch = $article['article_fetch'];
+        $articlesCategorize = $this->model->articlesCategorize();
+        $mostView = $this->model->mostView();
+        $newArticle = $this->model->newArticle();
 
-  }
+        $commentParameter = $this->model->commentParameter($id_articles, $id_articles_category);
+        $commentParameterName = $commentParameter['titel'];
+        $commentParameterScores = $commentParameter['scores_total'];
+        $comments = $this->model->comment($id_articles, $id_articles_category);
+        $data = [
+            'article_fetchAll' => $article_fetchAll,
+            'article_fetch' => $article_fetch,
+            'articlesCategorize' => $articlesCategorize,
+            'mostView' => $mostView,
+            'newArticle' => $newArticle,
+            'commentParameterName' => $commentParameterName,
+            'commentParameterScores' => $commentParameterScores,
+            'comments' => $comments
+        ];
+        $this->view('articles/index', $data);
+    }
+
+    function tab($id_articles, $id_articles_category) {
+        $number = $_POST['number'];
+        if ($number == 0) {
+            $commentParameter = $this->model->commentParameter($id_articles, $id_articles_category);
+            $commentParameterName = $commentParameter['titel'];
+            $commentParameterScores = $commentParameter['scores_total'];
+            $comments = $this->model->comment($id_articles, $id_articles_category);
+            $data = [
+                'commentParameterName' => $commentParameterName,
+                'commentParameterScores' => $commentParameterScores,
+                'comments' => $comments
+            ];
+            $this->view('articles/comment_results', $data, 1, 1);
+        }
+        if ($number == 1) {
+
+
+
+            $data = [
+            ];
+
+            $this->view('articles/comment_logs', $data, 1, 1);
+        }
+    }
+
 }
