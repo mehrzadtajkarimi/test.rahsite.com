eefd42ffaf3a115449f83747990fbfc3c77e4953
Mehrzad <mehrzadtajkarimi@gmail.com>
2021-05-13 11:36:00 +0430

    remove all last vertion

--- core/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('URL','http://test.me/');
-
-error_reporting(E_ALL);
-ini_set('display_errors',1);
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$data = [1=>2,2=>3,3=>5];
-$a= serialize($data);
-$bd= unserialize($a);
-print_r($a) ;
